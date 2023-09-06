77d9f195ef4a2e4bab5c0de636b974b059bdbb1f
Juan Jose <chambijuanjose05@gmail.com>
2023-09-05 23:01:05 -0300

    TxTs/ClientConfig

---
+++ Client/src/View/Register/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./Register.module.css"
+
+function Register() {
+  return (
+    <div className={style.viewRegister}>Register</div>
+  )
+}
+
+export default Register
