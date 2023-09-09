dfa3a0df9bf3b7a151adc243306cb9fcad80ada3
Juan Jose <chambijuanjose05@gmail.com>
2023-09-09 17:46:00 -0300

    TxTs/EndpointsUsers

--- Server/src/controlers/controlerUser.js
+++ Server/src/controlers/controlerUser.js
@@ -4,20 +4,20 @@ const router = Router();
 const bcrypt = require("bcryptjs")
 
 router.post("/signin", async (req, res) => {
+    const {email, contraseña} = req.body;
     try {
-        const {email, contraseña} = req.body;
-        if (email && contraseña) {
-            const emailMinus = email.toLowerCase()
-            const user = await User.findOne({where:{email:emailMinus}})
-            if (user) {
-               const value = await bcrypt.compare(contraseña, user.contraseña)
-               const result = value ? {message:"Aprobado"}: {message:"No Aprobado"}
-               return res.status(200).json(user)
-            }else{
-               return res.status(404).json("Usuario No encontrado")
-            }
+        if(!email) return res.status(404).json({message:"Email Faltante"});
+        if(!contraseña) return res.status(404).json({message:"Contraseña Faltante"});
+
+        const emailMinus = email.toLowerCase()
+        const user = await User.findOne({where:{email:emailMinus}})
+        if (user) {
+           const value = await bcrypt.compare(contraseña, user.contraseña)
+           const result = value ? {message:"Aprobado"}: {message:"No Aprobado"}
+        //    return res.status(200).json(user)
+           return res.status(200).json(result)
         }else{
-            res.status(404).json({message:"Falta de Credenciales"})
+           return res.status(404).json("Usuario No encontrado")
         }
     } catch (error) {
         res.status(500).json({error:error.message})
@@ -30,7 +30,8 @@ router.post("/checkin", async (req, res) => {
         if (nombre && apellido && nombreUsuario && email && contraseña) {
             const emailMinus = email.toLowerCase();
             const userExist = await User.findOne({where:{email: emailMinus}})
-            if (userExist) throw new Error("El Usuario Existe");
+            const userName = await User.findOne({where:{nombreUsuario: nombreUsuario}})
+            if (userExist || userName) throw new Error("El Usuario Existe");
             const contraseñaHashed = await bcrypt.hash(contraseña, 10);
             const userCreate = {
                 nombre:nombre,
@@ -40,8 +41,8 @@ router.post("/checkin", async (req, res) => {
                 contraseña: contraseñaHashed
             }
             const userCreated = await User.create(userCreate)
-            res.status(200).json(userCreated)
-            // res.status(200).json({message: "Usuario creado"})
+            // res.status(200).json(userCreated)
+            res.status(200).json({message: "Usuario creado"})
         }else{
             res.status(404).json({message:"Flata de Credenciales"})
         }
