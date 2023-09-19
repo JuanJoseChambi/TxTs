59acf080e5c490b545f4ba5f87c797760f81dbe6
Juan Jose <chambijuanjose05@gmail.com>
2023-09-18 22:50:18 -0300

    TxTs/PublicationsComponent

--- Client/src/Components/Publications/Publications.jsx
+++ Client/src/Components/Publications/Publications.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import NotUser from "../../assets/NotUser.png"
 import style from "./Publications.module.scss";
 
+
 function Publications() {
+
   return (
     <div className={style.publications}>
-      
+      <div className={style.headerPublication}>
+        <div className={style.containerImage}>
+          <img className={style.image} src={NotUser} alt="" />
+        </div>
+        <div>
+          <p>Juan Jose</p>
+          <p>@Jotta</p>
+        </div>
+      </div>
+      <div className={style.containerPublication}>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga obcaecati sequi delectus natus suscipit necessitatibus dignissimos excepturi sapiente, nam optio provident accusamus consectetur assumenda magni a illo maxime dolores exercitationem.</p>
+        <div className={style.spaceImage}>
+          <div className={style.containerImagePublications}>
+            <img className={style.imagePublication} src={NotUser} alt="" />
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
