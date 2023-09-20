2abb0fc8ddb21d29e92e84b625cc161971e7d855
Juan Jose <chambijuanjose05@gmail.com>
2023-09-20 18:33:27 -0300

    TxTs/CreatePost

--- Client/src/View/CreatePost/CreatePost.jsx
+++ Client/src/View/CreatePost/CreatePost.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import style from "./CreatePost.module.scss"
+import Button from "../../Components/Button/Button"
 
 
 function CreatePost() {
   return (
     <div className={style.viewCreatePost}>
         <div className={style.sectionCreate}>
-            <h2>Crear Publicacion</h2>
+            <h2 className={style.title}>Crear Publicacion</h2>
+            <div className={style.containerOptions}>
+                <textarea className={style.textarea} placeholder='Ej: Hola Hoy tuve un gran dia'></textarea>
+                <i class='bx bx-image-add'></i>
+            </div>
+            <Button text={"Crear Publicacion"}/>
         </div>
     </div>
   )
