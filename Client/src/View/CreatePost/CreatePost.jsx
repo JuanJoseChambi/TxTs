75c1962cb33d9f7a41fe0f498477755a72114b4d
Juan Jose <chambijuanjose05@gmail.com>
2023-09-22 20:40:14 -0300

    TxTs/CreatePost

--- Client/src/View/CreatePost/CreatePost.jsx
+++ Client/src/View/CreatePost/CreatePost.jsx
@@ -1,33 +1,59 @@
-import React, { useState, useRef } from 'react'
-import style from "./CreatePost.module.scss"
-import Button from "../../Components/Button/Button"
-
+import React, { useState, useRef } from 'react';
+import style from "./CreatePost.module.scss";
+import Button from "../../Components/Button/Button";
+import axios from 'axios';
+import { alertSuccess } from '../../Components/Alerts/Alerts';
 
 function CreatePost() {
   const [post, setPost] = useState({
     text:"",
     image:""
   })
-  const [file, setFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
+  const [imageAcept, setImageAcept] = useState(null);
+  const preset_key = "TxTsData";
+  const cloud_name = "dth62bdky";
+  const URL = `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`;
 
-  const handleFileChange = (e) => {
+  const handleFileChange = async (e) => {
     const selectedFile = e.target.files[0];
-
-    if (selectedFile) {
-      setFile(selectedFile);
-
-      const reader = new FileReader();
-      reader.onload = () => {
-        setPreviewImage(reader.result);
-      };
-
-      reader.readAsDataURL(selectedFile);
+    try {
+      if (selectedFile) {
+        // Vista previa de la image
+        const reader = new FileReader();
+        reader.onload = () => {
+          setPreviewImage(reader.result);
+        };
+        reader.readAsDataURL(selectedFile);
+        // Subir Imagen a la Cloudinary
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+        formData.append("upload_preset", preset_key);
+        setImageAcept(formData)
+      }
+    } catch (error) {
+        alert(error);
     }
   };
+  const info = JSON.parse(localStorage.getItem("info"))
+
+  async function handlerPost () {
+    const response = await axios.post(URL, imageAcept);
+    const secureUrl = response.data.secure_url;
+    setPost({ ...post, image: secureUrl});
+
+    if (post.text || post.image) {
+      const {data} = await axios.post(`/api/post/${info.id}`, post)
+      if (data.create) {
+        alertSuccess(data.message);
+      }else{
+        data.message
+      }
+    }
+  }
+
 
   const selectImage = useRef(null)
-
   function hanlderClickInput () {
     if (selectImage.current) {
       selectImage.current.click()
@@ -43,15 +69,14 @@ function CreatePost() {
                 <i onClick={hanlderClickInput} className='bx bx-image-add'></i>
                 <input type="file" style={{display:"none"}} onChange={handleFileChange} ref={selectImage}/>
             </div>
-            <div className={style.sectionPreviewImage}>
-              <div className={style.containerImage}>
+            {previewImage
+            ? <div className={style.sectionPreviewImage}>
+                <div className={style.containerImage}>
                   {previewImage? <img className={style.image} src={previewImage?previewImage:null} alt="" /> : null}
-                  {/* <p>{file.name?file.name:null}</p> */}
-                  {/* <p>{file.type?file.type:null}</p> */}
-                  {/* <p>{file.size?file.size:null}</p> */}
+                </div>
               </div>
-            </div>
-            <Button text={"Crear Publicacion"}/>
+            :null}
+            <Button onClick={handlerPost} text={"Crear Publicacion"}/>
         </div>
     </div>
   )
