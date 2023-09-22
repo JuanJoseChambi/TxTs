81afc8e22532d6d6b8fbd35fb2cc37645ff9b68f
Juan Jose <chambijuanjose05@gmail.com>
2023-09-22 19:50:30 -0300

    TxTs/CreatePost

--- Client/src/View/CreatePost/CreatePost.jsx
+++ Client/src/View/CreatePost/CreatePost.jsx
@@ -9,17 +9,17 @@ function CreatePost() {
     image:""
   })
   const [file, setFile] = useState(null);
-  const [previewUrl, setPreviewUrl] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
-    
+
     if (selectedFile) {
       setFile(selectedFile);
 
       const reader = new FileReader();
       reader.onload = () => {
-        setPreviewUrl(reader.result);
+        setPreviewImage(reader.result);
       };
 
       reader.readAsDataURL(selectedFile);
@@ -42,10 +42,14 @@ function CreatePost() {
                 <textarea className={style.textarea} placeholder='Ej: Hola Hoy tuve un gran dia' onChange={(e) => setPost({...post, text:e.target.value})}></textarea>
                 <i onClick={hanlderClickInput} className='bx bx-image-add'></i>
                 <input type="file" style={{display:"none"}} onChange={handleFileChange} ref={selectImage}/>
-                <p>{file.name?file.name:null}</p>
-                <p>{file.type?file.type:null}</p>
-                <p>{file.size?file.size:null}</p>
-                <img src={previewUrl?previewUrl:null} alt="" />
+            </div>
+            <div className={style.sectionPreviewImage}>
+              <div className={style.containerImage}>
+                  {previewImage? <img className={style.image} src={previewImage?previewImage:null} alt="" /> : null}
+                  {/* <p>{file.name?file.name:null}</p> */}
+                  {/* <p>{file.type?file.type:null}</p> */}
+                  {/* <p>{file.size?file.size:null}</p> */}
+              </div>
             </div>
             <Button text={"Crear Publicacion"}/>
         </div>
