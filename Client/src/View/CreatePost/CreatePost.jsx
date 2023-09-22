09b6955d3123e0902c9133057e9465478c63ddee
Juan Jose <chambijuanjose05@gmail.com>
2023-09-22 19:23:00 -0300

    TxTs/ProfileImage

--- Client/src/View/CreatePost/CreatePost.jsx
+++ Client/src/View/CreatePost/CreatePost.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import style from "./CreatePost.module.scss"
 import Button from "../../Components/Button/Button"
 
 
 function CreatePost() {
+  const [post, setPost] = useState({
+    text:"",
+    image:""
+  })
+  const [file, setFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    
+    if (selectedFile) {
+      setFile(selectedFile);
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreviewUrl(reader.result);
+      };
+
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  const selectImage = useRef(null)
+
+  function hanlderClickInput () {
+    if (selectImage.current) {
+      selectImage.current.click()
+    }
+  }
+
   return (
     <div className={style.viewCreatePost}>
         <div className={style.sectionCreate}>
             <h2 className={style.title}>Crear Publicacion</h2>
             <div className={style.containerOptions}>
-                <textarea className={style.textarea} placeholder='Ej: Hola Hoy tuve un gran dia'></textarea>
-                <i class='bx bx-image-add'></i>
+                <textarea className={style.textarea} placeholder='Ej: Hola Hoy tuve un gran dia' onChange={(e) => setPost({...post, text:e.target.value})}></textarea>
+                <i onClick={hanlderClickInput} className='bx bx-image-add'></i>
+                <input type="file" style={{display:"none"}} onChange={handleFileChange} ref={selectImage}/>
+                <p>{file.name?file.name:null}</p>
+                <p>{file.type?file.type:null}</p>
+                <p>{file.size?file.size:null}</p>
+                <img src={previewUrl?previewUrl:null} alt="" />
             </div>
             <Button text={"Crear Publicacion"}/>
         </div>
