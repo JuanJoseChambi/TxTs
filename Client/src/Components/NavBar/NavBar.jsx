7e361d49804aa9ff8e8c3aa3744c87dbad8edd0e
Juan Jose <chambijuanjose05@gmail.com>
2023-09-29 16:13:58 -0300

    TxTs/SearchUser

--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./NavBar.module.scss";
 import TxTsNavBarLogo from "../../assets/TxTsNavBarLogo.png"
 import { useSelector } from "react-redux";
 import NotUser from "../../assets/NotUser.png"
+import axios from "axios"
 
 function NavBar() {
+  const [searchPost, setSearchPost] = useState({
+    searchPost:""
+  })
+
+  async function handlerSearchPost () {
+    const {data} = await axios.get(`/api/post?searchPost=${searchPost.searchPost}`)
+    // console.log(data);
+
+  }
+  function handlerSend (e) {
+    if (e.key === "Enter") {
+      handlerSearchPost()
+      e.preventDefault()
+    }
+  }
 
   const {infoUser} = useSelector(state => state.info)
 
@@ -17,7 +33,7 @@ function NavBar() {
 
       <form className={style.inputContainer}>
         <label className={style.icon}><i className='bx bx-search-alt-2'></i></label>
-        <input className={style.input} name="search" type="text" placeholder="Buscar"/>
+        <input className={style.input} name="search" type="text" placeholder="Buscar" onKeyDown={(e) => handlerSend(e)} onChange={(e) => setSearchPost({searchPost:e.target.value})}/>
       </form>
 
       <div className={style.infoUser}>
